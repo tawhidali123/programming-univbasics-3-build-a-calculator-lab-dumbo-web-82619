8f27907165d9b4ebfb12ad2daea2748200514e92
Tawhid Ali <tawhidali126@gmail.com>
2019-07-30 01:06:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,13 @@
 def addition(num1, num2)
-
+  return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  return num1 - num2
 end
 
 def division(num1, num2)
-
+  return num1 / num2
 end
 
 def multiplication(num1, num2)
